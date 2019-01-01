bfe499dde02a3fb7f8c3da288925eb1ca5e2d76a
Steven <gerick@kth.se>
2019-01-01 18:53:09 +0100

    Few changes to classes of amino acids. Added a state for 2nd and 10th amino acids

--- sigclassify.py
+++ sigclassify.py
@@ -9,39 +9,48 @@ lexicon = {
 'R': 0, 'H': 0, 'K': 0, # positive
     'D': 1, 'E': 1, #negative
     'S': 8, 'T': 2, 'N': 2, 'Q': 2, #polar uncharged
-    'G': 4, 'P': 9, #special
+    'G': 4, 'P': 10, #special
     'C': 5, 'A': 5, 'V': 6, 'I': 7, #hydrophobic
     'L': 3, 'M': 3, 'F': 3, 'Y': 9, 'W': 8, #hydrophobic
-    'X': 10 #unknown
+    'X': 12 #unknown
 } #0 positive, 1 negative, 2 polar uncharged, 3 special, 4 hydrophobic, 5 unknown. Cysteine counted as hydrophobic
 num_emissions = lexicon['X'] + 1
 
 annot_to_state = {
-    'n': 0,
-    'h': 1,
-    'c': 2,
-    'v': 3,
-    'g': 4, #not present in original annotations    
-    'C': 5,
-    'f': 6, #not present in original annotations
-    'O': 6,
-    'o': 7,
-    'i': 8,
-    'M': 9,
+    's': 0,
+    't': 1,
+    'S': 2,
+    'n': 3,
+    'h': 4,
+    'c': 5,
+    'V': 6,
+    'v': 7,
+    'g': 8, #not present in original annotations    
+    'C': 9,
+    'f': 10, #not present in original annotations
+    'O': 11,
+    'o': 12,
+    'i': 13,
+    'M': 14,
 }
 num_states = annot_to_state['M'] + 1;
 
 state_to_annot = {
-    0: 'n',
-    1: 'h',
-    2: 'c',
-    3: 'g',
-    4: 'C',
-    5: 'f',
-    6: 'O',
-    7: 'o',
-    8: 'i',
-    9: 'M',
+    0: 's',
+    1: 't',
+    2: 'S',
+    3: 'n',
+    4: 'h',
+    5: 'c',
+    6: 'V',
+    7: 'v',
+    8: 'g',
+    9: 'C',
+    10: 'f',
+    11: 'O',
+    12: 'o',
+    13: 'i',
+    14: 'M'
 
 }
 
@@ -58,10 +67,12 @@ pos_non_tm = 2
 pos_tm = 3
 
 class Sequence:
-    def __init__(self, name, seq, comment):
+    def __init__(self, name, seq, lexiconseq, label, stateseq):
         self.name = name
         self.seq = seq
-        self.comment = comment
+        self.lexiconseq = lexiconseq
+        self.label = label
+        self.stateseq = stateseq
 
 def calc_probs(directories):
     pass
@@ -82,6 +93,7 @@ def main():
     if not os.path.isdir(d_pos_tm):
         print(d_pos_tm + " does not exist.")
         return
+    lim_seq = 400 #max characters to read from sequence
     examples = [[], [], [], []]
     id_correct = 0
     id_wrong = 0
@@ -95,40 +107,55 @@ def main():
         for file in os.listdir(example_directories[i]):
             lines = open(example_directories[i] + "/" + file, 'r').readlines()
             for j in range(0, len(lines), 3):
-                seq = Sequence(lines[j].split(' ')[0][1:], lines[j+1].strip()[0:100], lines[j+2][1:].strip())
-                examples[i].append(seq)
+                name = lines[j].split(' ')[0][1:]
+                sequence = lines[j+1].strip()[0:lim_seq]
+                stateseq = []
+                lexiconseq = []
+                comment = lines[j+2][1:].strip()                
                 prevstate = None
-                for k in range(0, len(seq.seq)):
+                
+                for k in range(0, len(sequence)):
                     try:
-                        state = annot_to_state[seq.comment[k]]
-                        if seq.comment[k] == 'c' and (seq.comment[k+1] == 'C'):
+                        state = annot_to_state[comment[k]]
+                        if k == 0 and i >= 2:
+                            letter = 's'
+                            state = annot_to_state[letter]
+                            comment = comment[:k] + letter + comment[k+1:]
+                        elif k == 1 and i >= 2:
+                            letter = 'S'
+                            state = annot_to_state[letter]
+                            comment = comment[:k] + letter + comment[k+1:]
+                        elif (comment[k] == 'i') and len(comment) > k+1 and comment[k+1] != comment[k]:
+                            letter = 't'
+                            state = annot_to_state[letter]
+                            comment = comment[:k] + letter + comment[k+1:]
+                        elif comment[k] == 'c' and (comment[k+1] == 'C'):
                             state = annot_to_state['g']
-                            mod_string = list(seq.comment)
-                            mod_string[k] = 'g'
-                            seq.comment = ''.join(mod_string)
-                            #print(seq.name + ":" + seq.seq[k:k+3])
-                        elif seq.comment[k] == 'c' and (seq.comment[k+2] == 'C'):
+                            comment = comment[:k] + 'g' + comment[k+1:]
+                        elif comment[k] == 'c' and (comment[k+2] == 'C'):
                             state = annot_to_state['v']
-                            mod_string = list(seq.comment)
-                            mod_string[k] = 'v'
-                            seq.comment = ''.join(mod_string)
-                            #print(seq.name + ":" + seq.seq[k:k+3])
-                        elif (seq.comment[k] == 'O' or seq.comment[k] == 'o') and (seq.comment[k-1] == 'C'):
+                            comment = comment[:k] + 'v' + comment[k+1:]
+                        elif (comment[k] == 'c' or comment[k] == 'h' or comment[k] == 'n') and (k == 10):
+                            state = annot_to_state['V']
+                            comment = comment[:k] + 'V' + comment[k+1:]
+                        elif (comment[k] == 'O' or comment[k] == 'o') and (comment[k-2] == 'C'):
                             state = annot_to_state['f']
-                            mod_string = list(seq.comment)
-                            mod_string[k] = 'f'
-                            seq.comment = ''.join(mod_string)
+                            comment = comment[:k] + 'f' + comment[k+1:]
                     except KeyError as e:
                         print(e)
-                        print(seq.comment)
+                        print(comment)
                         return
                     if k == 0:
                         start[state] += 1
                     else:
                         transitions[prevstate][state] += 1
-                        emissions[state][lexicon[seq.seq[k]]] += 1
-                        
+                    em = lexicon[sequence[k]]
+                    lexiconseq.append(em)
+                    emissions[state][em] += 1                        
                     prevstate = state
+                    stateseq.append(state)
+                seq = Sequence(name, sequence, lexiconseq, comment, stateseq)
+                examples[i].append(seq)
     #transitions[3] = np.ones([num_states])
     #print(transitions)
     sum_trans = np.sum(transitions, 1)[:, None]
@@ -138,11 +165,13 @@ def main():
     transitions = transitions / sum_trans
     emissions = emissions / sum_ems
     start = start / np.sum(start)
-    #print(start)
     #print(transitions)
     #print(emissions)
     model.startprob = start
     model.startprob_ = start
+    
+    print(model.startprob)
+    print(model.startprob_)
     model.emissionprob = emissions
     model.emissionprob_ = emissions
     model.transmat = transitions
@@ -151,31 +180,43 @@ def main():
     #print(model.n_components)
     #print(model.startprob_)
     
+    chars_per_line = 80
     for i in range(4):
         num_correct = 0
         num_incorrect = 0
         for seq in examples[i]:
-            classes = []
-            for character in seq.seq:
-                classes.append(lexicon[character])
-            
-            logprob, state_seq_enc = model.decode(np.transpose([classes[0:100]]))
+            logprob, state_seq_enc = model.decode(np.transpose([seq.lexiconseq]))
+            logprob2, state_seq_enc2 = model.decode(np.transpose([seq.lexiconseq[0:60]]))            
             state_seq = []
             for digit in state_seq_enc:
                 state_seq.append(state_to_annot[digit])
             #print(seq.name)
+            stateseq2 = state_to_annot[state_seq_enc2[0]]
+            stateseqstr = ''.join(state_seq)
             #print(''.join(state_seq))
-            #print(seq.comment)
-            predicted = state_seq[0] == 'n' and seq.seq[0] == 'M'
-            correct = predicted == (i >= 2)
+            #print(seq.label)
+            predicted = state_seq[0] == 's' and stateseq2 == 's'
+            correct = (predicted == (i >= 2))
             num_incorrect += (1 - correct)
             num_correct += correct
-            if not correct:
-                #print(seq.name)
-                #print(seq.seq)
-                #print(''.join(state_seq))
-                #print(seq.comment)
-                pass
+            if predicted:
+                logprob3, state_seq_enc3 = model.decode(np.transpose([seq.lexiconseq[0:stateseqstr.find('C')]]))
+                state_seq3 = []
+                for digit in state_seq_enc3:
+                    state_seq3.append(state_to_annot[digit])
+                if not correct:
+                    print(''.join(state_seq3))
+            debug = False
+            if not correct and debug:
+                j = 0
+                print(seq.name)                
+                while (j < len(seq.seq)):
+                    num_print = min(chars_per_line, len(seq.seq) - chars_per_line)
+                    print(seq.seq[j:j+num_print])
+                    print(stateseqstr[j:j+num_print])
+                    print(seq.label[j:j+num_print])
+                    print('')
+                    j += chars_per_line
         print("{0} correct, {1} incorrect. Accuracy {2}%".format(num_correct, num_incorrect, num_correct * 100 / (num_correct + num_incorrect)))
         
             
